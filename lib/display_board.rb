3031a3853b8f0b4b72e3fffa140ed84d2f6ede03
Trish Ogen <patriciaogen@gmail.com>
2019-05-08 01:41:14 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  puts "  |  |  "
-  puts 
+  spots = "  |  |  "
+  line = "-----------"
+  puts spots line spots line spots
 end
